b542e6aadff33f239285e1f578dfefa0baa6008f
DESKTOP-UJO5UUV\DELL <yachita.poohping@mail.kmutt.ac.th>
2021-12-17 21:02:30 +0700

    city

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace City4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            while (true)
+            {
+                string namecity = Console.ReadLine();
+            }
+            
+          
+
+
+
+
+        }
+    }
+}
